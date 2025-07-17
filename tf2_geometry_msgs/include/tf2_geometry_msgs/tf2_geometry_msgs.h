d45288c334865da411a588790c2b1b672c80b6d7
Silvio Traversaro <silvio@traversaro.it>
2025-07-17 17:11:22 +0200

    Update tf2_geometry_msgs.h
    Signed-off-by: Silvio Traversaro <silvio@traversaro.it>

--- tf2_geometry_msgs/include/tf2_geometry_msgs/tf2_geometry_msgs.h
+++ tf2_geometry_msgs/include/tf2_geometry_msgs/tf2_geometry_msgs.h
@@ -32,8 +32,6 @@
 #ifndef TF2_GEOMETRY_MSGS__TF2_GEOMETRY_MSGS_H_
 #define TF2_GEOMETRY_MSGS__TF2_GEOMETRY_MSGS_H_
 
-#warning This header is obsolete, please include tf2_geometry_msgs/tf2_geometry_msgs.hpp instead
-
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 #endif  // TF2_GEOMETRY_MSGS__TF2_GEOMETRY_MSGS_H_
